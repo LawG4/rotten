92fd59a511fbb5bb6f3ce8f4c1d605c8224ce3bc
Lawrence <61335367+LawG4@users.noreply.github.com>
2022-10-24 00:18:40 +0100

    Add cpp header guard

--- inc/rotten/rotten-core.h
+++ inc/rotten/rotten-core.h
@@ -9,5 +9,16 @@
  */
 #ifndef __ROTTEN_EXTERNAL_CORE_H__
 #define __ROTTEN_EXTERNAL_CORE_H__ (1)
+#include "rotten-enum.h"
 
+// Cpp header guards, so that cpp projects can use our c libraries
+#ifdef __cplusplus
+#define ROTTEN_CPP_GUARD extern "C" {
+#define ROTTEN_CPP_GUARD_END }
+#else
+#define ROTTEN_CPP_GUARD
+#define ROTTEN_CPP_GUARD_END
+#endif  // !__cplusplus
+ROTTEN_CPP_GUARD
+ROTTEN_CPP_GUARD_END
 #endif  // !__ROTTEN_EXTERNAL_CORE_H__
