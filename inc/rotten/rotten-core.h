6d180f3ed9116a9b251fd0dd0a79439f5e944703
Lawrence <61335367+LawG4@users.noreply.github.com>
2022-10-23 21:41:10 +0100

    Add in rotten core project

---
+++ inc/rotten/rotten-core.h
@@ -0,0 +1,13 @@
+/***
+ * Rotten - Rotten-Core.h
+ * Provides functions which are shared in most rotten projects like the logging functionality. Including
+ * common macros which will export function symbols
+ *
+ * License   : MIT
+ * Copyright : Rotten
+ * Authors   : Lawrence G,
+ */
+#ifndef __ROTTEN_EXTERNAL_CORE_H__
+#define __ROTTEN_EXTERNAL_CORE_H__ (1)
+
+#endif  // !__ROTTEN_EXTERNAL_CORE_H__
